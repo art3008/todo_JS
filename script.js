73aa6b77032fc881fccfccb5ca14353933eb7698
art3008 <art.osv@mail.ru>
2022-11-02 12:16:23 +0600

    dasdads

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log(2);
+const todo = document.getElementById('to_do_list')
+const usrInp = document.getElementById('usrInp')
+
+usrInp.addEventListener('keydown', (event) => {
+    if(event.key == 'Enter'){
+        usrInp.value.length === 0 ? alert('Поле ввода пустое') : add()
+    }
+})
+
+function add() {
+    let div = document.createElement('div')
+    let h2 = document.createElement('h2')
+    let divBtn = document.createElement('div')
+    let btnRemove = document.createElement('button')
+
+    // btnRemove.style.width = '50px'
+    btnRemove.textContent = 'Remove'
+    btnRemove.className = 'divBtn'
+    div.className = 'item'
+
+    h2.innerHTML = usrInp.value 
+    
+
+    div.appendChild(h2)
+    div.appendChild(divBtn)
+    divBtn.appendChild(btnRemove)
+    
+    todo.appendChild(div)
+    
+    usrInp.value = ''
+
+
+    btnRemove.addEventListener('click', ()=> {
+        div.remove()
+    })
+
+}
